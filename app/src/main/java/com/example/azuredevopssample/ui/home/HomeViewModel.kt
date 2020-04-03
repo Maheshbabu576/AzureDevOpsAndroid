e1076b77cb29e87725ff75836e4363b3dd1810d9
md.saudahmed@gmail.com <Saifuddin@1>
2020-04-03 15:23:48 +0530

    changes to 65 by saud

--- app/src/main/java/com/example/azuredevopssample/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/azuredevopssample/ui/home/HomeViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class HomeViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home screen test changes 55"
+        value = "This is home screen test changes 65"
     }
     val text: LiveData<String> = _text
 }
