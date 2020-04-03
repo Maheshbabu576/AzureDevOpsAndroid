9dd0122e73ab4030358b1f5eb2bc61b9177ae129
Mahesh Babu <maheshbabu.576@outlook.com>
2020-04-03 14:58:04 +0530

    Updates

--- app/src/main/java/com/example/azuredevopssample/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/azuredevopssample/ui/home/HomeViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class HomeViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home screen test changes 1"
+        value = "This is home screen test changes 2"
     }
     val text: LiveData<String> = _text
 }
